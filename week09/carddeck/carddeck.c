ef38189740e86a4c99f699a9a79de6f454912f83
Carson <futgasansoo@gmail.com>
2023-03-17 13:58:25 -0400

    deal card function

--- week09/carddeck/carddeck.c
+++ week09/carddeck/carddeck.c
@@ -40,8 +40,17 @@ void shuffle(char deck[DECK_SIZE][CARD_SIZE])
     }
 }
 
-void deal_card(char deck[DECK_SIZE][CARD_SIZE], char card[CARD_SIZE]) // returns first non-blank card in deck
+void deal_card(char deck[DECK_SIZE][CARD_SIZE], char card[CARD_SIZE])
 {
+    for (int i = 0; i < DECK_SIZE; i++)
+    {
+        if (deck[i][0] != NULL && deck[i][1] != NULL)
+        {
+            card[0] = deck[i][0];
+            card[1] = deck[i][1];
+            break;
+        }
+    }
 }
 
 void deal_hand(char deck[DECK_SIZE][CARD_SIZE], char hand[][CARD_SIZE], int hand_size) // returns hand of hand_size
--- week09/carddeck/playing_with_cards.c
+++ week09/carddeck/playing_with_cards.c
@@ -4,9 +4,11 @@
 
 int main()
 {
+    char card[3];
     char deck[DECK_SIZE][CARD_SIZE];
     deck_factory(deck);
     shuffle(deck);
+    deal_card(deck, card);
 
     for (int i = 0; i < DECK_SIZE; i++)
     {
@@ -14,6 +16,7 @@ int main()
         printf("%c", deck[i][1]);
         printf("\n");
     }
+    printf("your card is: %c%c", card[0], card[1]);
 }
 
 #endif
